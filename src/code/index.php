ab6247551ce28d45270c218c26ce8bf48fa80781
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2021-10-03 23:59:15 +0900

    feat(OSCi): cookieでOSCiを実装

--- src/code/index.php
+++ src/code/index.php
@@ -1,35 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <title>sunflower</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sunflower</title>
 </head>
-
 <body>
-    <ul>
-        <li>
-            <a href="./404">OSCi in url Path</a>
-        </li>
-        <li>
-            <a href="./OSCiQP.php">OSCi in url Query Param</a>
-        </li>
-        <li>
-            <a href="./OSCiQV.php">OSCi in url Query Value</a>
-        </li>
-        <li>
-            <a href="./OSCiUA.php">OSCi in User-Agent</a>
-        </li>
-        <li>
-            <a href="./OSCiReferer.php">OSCi in Referer</a>
-        </li>
-        <li>
-            <a href="./OSCiBP.php">OSCi in Body Param</a>
-        </li>
-        <li>
-            <a href="./OSCiBV.php">OSCi in Body Value</a>
-        </li>
-    </ul>
+    <li>
+        <a href="OSCi/">OS Command injection</a>
+    </li>
 </body>
-
 </html>
