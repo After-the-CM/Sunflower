a4e310ee5533f9d3a6f1d36d8c758610b5d16193
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2021-12-07 20:03:05 +0900

    feat(stored): 入力と反映が同じstored

--- src/code/xss/index.php
+++ src/code/xss/index.php
@@ -1,20 +1,53 @@
+<?php
+$db = new PDO('mysql:host=mysql;dbname=sunflower', 'user', 'pass');
+
+if (isset($_POST["name"])) {
+    try {
+        $sql = $db->prepare('INSERT INTO memos values(:name, :memo);');
+        $sql->bindValue(':name', $_POST["name"], PDO::PARAM_STR);
+        $sql->bindValue(':memo', $_POST["memo"], PDO::PARAM_STR);
+        $sql->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+}
+
+try {
+    $sql = 'SELECT name,memo FROM memos;';
+    $result = $db->query($sql);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>rel trav</title>
+    <title>form</title>
 </head>
 
 <body>
-    <li>
-        <a href="form.php">form for stored XSS</a>
-    </li>
-    <li>
-        <a href="storedXSS.php">Stored XSS</a>
-    </li>
+    <p>plz input <span style="color:red;">&lt;script&gt;alert(1)&lt;/script&gt;</span></p>
+    <form method="POST">
+        name <input type="text" name="name" size="50">
+        memo <input type="text" name="memo" size="50">
+        <input type="submit" value="送信">
+    </form>
+    <hr>
+    <table border="1">
+        <tr>
+            <th style="width: 200px">name</th>
+            <th style="width: 500px">memo</th>
+        </tr>
+        <?
+        foreach ($result as $value) {
+            print "<tr><td>$value[name]</td><td>$value[memo]</td></tr>";
+        }
+        ?>
+    </table>
 </body>
 
 </html>
