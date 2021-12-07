abd036e1dc8082ec9e9948fd1f8cba633541e667
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2021-12-07 20:12:35 +0900

    fix:

--- src/code/404.php
+++
@@ -1,7 +0,0 @@
-<p>plz redirect to <span style="color:red;">/a;sleep 5</span></p>
-<?php
-print "<p>current url.Path: " . urldecode($_SERVER['REQUEST_URI']). "</p>";
-exec("cat " . $_SERVER['DOCUMENT_ROOT'] . urldecode($_SERVER['REQUEST_URI']), $output);
-foreach ($output as $line) {
-    print($line);
-}
